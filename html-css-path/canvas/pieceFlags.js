d58f264b0ee73f9f146bfb7ed926824b2f83d645
alexNeto <alex.neto3@hotmail.com>
2017-10-17 05:20:25 -0200

    DRY no pieceFlag.js

--- html-css-path/canvas/pieceFlags.js
+++ html-css-path/canvas/pieceFlags.js
@@ -1,63 +1,39 @@
 function upperHalf(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(0, 0, 360, 120);
-    context.closePath()
-    context.fill()
+    context.fillRect(0, 0, 360, 120)
 }
 
 function lowerHalf(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(0, 120, 360, 120);
-    context.closePath()
-    context.fill()
+    context.fillRect(0, 120, 360, 120)
 }
 
 function upperThird(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(0, 0, 360, 80);
-    context.closePath()
-    context.fill()
+    context.fillRect(0, 0, 360, 80)
 }
 
 function middleThird(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(0, 80, 360, 80);
-    context.closePath()
-    context.fill()
+    context.fillRect(0, 80, 360, 80)
 }
 
 function lowerThird(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(0, 160, 360, 80);
-    context.closePath()
-    context.fill()
+    context.fillRect(0, 160, 360, 80)
 }
 
 function right(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(0, 0, 120, 240);
-    context.closePath()
-    context.fill()
+    context.fillRect(0, 0, 120, 240)
 }
 
 function center(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(120, 0, 120, 240);
-    context.closePath()
-    context.fill()
+    context.fillRect(120, 0, 120, 240)
 }
 
 function left(color){
     context.fillStyle = color
-    context.beginPath()
-    context.rect(240, 0, 120, 240);
-    context.closePath()
-    context.fill()
+    context.fillRect(240, 0, 120, 240);
 }
