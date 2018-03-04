70d485164de2a61b381b808a413a2f739eb7c3b7
alexNeto <alex.neto3@hotmail.com>
2018-03-04 19:46:12 -0300

    loops

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,10 +2,13 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
 
 
-var primeiroPaciente = document.querySelector("#primeiro-paciente");
+var paciente = document.querySelectorAll(".paciente");
+for (var i = 0; i < paciente.length; i++) {
+    var peso = paciente[i].querySelector(".info-peso").textContent;
+    var altura = paciente[i].querySelector(".info-altura").textContent;
+    var imc = peso / (altura * altura);
+    var infoImc = paciente[i].querySelector(".info-imc");
 
-var peso = primeiroPaciente.querySelector(".info-peso").textContent;
-var altura = primeiroPaciente.querySelector(".info-altura").textContent;
-var imc = peso / (altura * altura);
-var infoImc = primeiroPaciente.querySelector(".info-imc");
-infoImc.textContent = imc;
+    infoImc.textContent = imc.toFixed(2);
+}
+
